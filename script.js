838bf777760e68aff8d4057ba134c91512debc33
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-07-01 13:49:53 -0700

    I added a bunch of variables. Some with prompts asking for the criteria of the password and others to store an array of possible values to pull from for the password. I also converted the variables with the prompts to upper case in order to eliminate any possible confusion.

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["!", "@", "#", "$", "%", "&", "*", "+"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var charLength = window.prompt("How many characters?");
+var lowerCase = window.prompt("Do you want lower cases? Type Yes or No");
+var upperCase = window.prompt("Do you want upper cases? Type Yes or No");
+var numChar = window.prompt("Do you want numbers? Type Yes or No");
+var specChar = window.prompt("Do you want special characters? Type Yes or No");
+lowerCase.toUpperCase();
+upperCase.toUpperCase();
+numChar.toUpperCase();
+specChar.toUpperCase();
