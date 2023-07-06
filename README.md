fa9bbaed652610e6001ec5a8643c3e78db67067d
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-07-05 18:21:08 -0700

    done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # random-password-generator
 App that generates a random password that complies to certain criteria
+![ScreenShot](assets/Screen%20Shot%202023-07-05%20at%206.15.46%20PM.png?raw=true "Screenshot")
--- script.js
+++ script.js
@@ -17,29 +17,40 @@ var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var special = ["!", "@", "#", "$", "%", "&", "*", "+"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var possibleCharacters = [lettersLower[Math.floor(Math.random() * lettersLower.length)], numbers[Math.floor(Math.random()*numbers.length)], special[Math.floor(Math.random()*special.length)], lettersUpper[Math.floor(Math.random()*lettersUpper.length)]];
+
 var charLength = window.prompt("How many characters?");
-var lowerCase = window.prompt("Do you want lower cases? Type Yes or No");
-var upperCase = window.prompt("Do you want upper cases? Type Yes or No");
-var numChar = window.prompt("Do you want numbers? Type Yes or No");
-var specChar = window.prompt("Do you want special characters? Type Yes or No");
-lowerCase.toUpperCase();
-upperCase.toUpperCase();
-numChar.toUpperCase();
-specChar.toUpperCase();
-function generatePassword() {
+var lowerCase = window.prompt("Do you want lower cases? Type true or false");
+var upperCase = window.prompt("Do you want upper cases? Type true or false");
+var numChar = window.prompt("Do you want numbers? Type true or false");
+var specChar = window.prompt("Do you want special characters? Type true or fasle");
+
+lowerCase=(lowerCase==="true");
+upperCase=(upperCase==="true");
+numChar=(numChar==="true");
+specChar=(specChar==="true");
+
   var tryAgain = "Character length must be between 8-128 characters. Please try again by refreshing";
-  if(charLength< 8 || charLength>128){
+  
+  var valueOfCharLength = Number(charLength);
+
+  function generatePassword() {
    
-    return tryAgain
-  }
-  // if(lowerCase="YES"){
-  //   for(var i=0; i< charLength;i++) {
-  //     var lowerPassword=lettersLower[Math.floor(Math.random() * charLength)]
-  //   }
-  //   return tryAgain
-  // }
- 
-  }
-  var randomArray= possibleCharacters[Math.floor(Math.random()*possibleCharacters.length)];
-  console.log(randomArray);
+    var availableCharacters = [
+      ...(lowerCase ? lettersLower: []),
+      ...(upperCase ? lettersUpper: []),
+      ...(numChar ? numbers: []),
+      ...(specChar ? special: []),
+    ];
+    var generatedPassword="";
+    
+    if(charLength< 8 || charLength>128){
+       
+      return tryAgain
+    }
+    for(var i=0; i<valueOfCharLength;i++){
+      var randomCharacter= Math.floor(Math.random()*availableCharacters.length);
+      generatedPassword+= availableCharacters[randomCharacter];
+    }
+    return generatedPassword;
+     }
+     generatePassword()
